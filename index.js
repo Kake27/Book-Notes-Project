5bfde5b0bca84a8f99bbcb4591faeb45f4b97d8e
BattleGear04 <karekarayoosh@gmail.com>
2024-08-13 23:10:14 +0530

    environment variables added

--- index.js
+++ index.js
@@ -33,9 +33,9 @@ function parseQuery(response) {
 
 
 const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "bookrecords",
+    user: process.env.DATABASE_USER,
+    host: process.env.DATABASE_HOST,
+    database: process.env.DATABASE_NAME,
     password: process.env.DATABASE_PASSWORD,
     port: 5432
 })
